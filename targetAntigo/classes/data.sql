247198d6037b3f7516c7453eaa5c8bbfbbb4150c
Luiz Felipe <luizfelipebasile@gmail.com>
2020-05-31 23:29:08 -0300

    Removendo pasta desnecessária

--- targetAntigo/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO category (name, status) values ("Parque", 1);
-INSERT INTO category (name, status) values ("Museu", 1);
-INSERT INTO category (name, status) values ("Teatro", 1);
-INSERT INTO category (name, status) values ("Monumento", 1);
